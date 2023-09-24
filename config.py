9ad0e642ba456c99db086c8cfa4c3cdd8264be06
Hussein <118964436+SS7SS@users.noreply.github.com>
2023-09-24 14:48:45 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+class Config:
+    API_ID = 7715737 #خلي مثل ماهوه
+    API_HASH = "241e23bd490bfad03f0da62d4f5d8a20" #خلي مثل ماهوه
+    TOKEN = "59758258166:AAEuANb_008uAgVJwQWeDKUYlK5qBMuN8MMT0"  #توكن
+    START_PIC = "https://graph.org/file/9ffcff556e24e0c37e444.jpg" #هنا حط رابط صورة تلكراف
+    CHAT = "-1001852789435" #وهنا خلي ايدي كروب التخزين 
+
+
+
